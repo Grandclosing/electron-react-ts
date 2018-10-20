68c6c27ade931798407b9c0ab7cf972a4b134dcc
Grandclosing <vukpetrovic1@yahoo.com>
2018-10-20 14:48:17 -0400

    Modified App.tsx

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -11,8 +11,11 @@ class App extends React.Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+
+        { /* The path to App.tsx is a little convoluted, but used to demonstrate the use of React.js, 
+             es6 template strings, and the use of the Node.js __dirname variable from the Electron renderer process. */ }        
         <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+          To get started, edit <code>{`${__dirname}src/Components/App/App.tsx`}</code> and save to reload!
         </p>
       </div>
     );
